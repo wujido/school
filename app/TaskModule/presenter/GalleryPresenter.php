4c5cd47428a0ff7eb384aeca96a3cf5a47f9b26e
wujido <wujido20@gmail.com>
2019-01-13 23:23:08 +0100

    Gallery

---
+++ app/TaskModule/presenter/GalleryPresenter.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Václav Hrouda <vahrouda@gmail.com>.
+ * Date: 13.1.19
+ */
+
+namespace App\Presenters;
+
+
+use App\Model\GalleryManager;
+
+class GalleryPresenter extends BasePresenter
+{
+
+	/**
+	 * @var GalleryManager
+	 */
+	private $galleryManager;
+
+	public function __construct(GalleryManager $galleryManager)
+	{
+		$this->galleryManager = $galleryManager;
+	}
+
+	public function renderDefault()
+	{
+		$this->template->images = $this->galleryManager->getImages();
+	}
+
+	public function renderDetail($name)
+	{
+		$this->template->images =  $this->galleryManager->getImagesByName($name);
+	}
+
+
+}
---
+++ app/model/GalleryManager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Václav Hrouda <vahrouda@gmail.com>.
+ * Date: 13.1.19
+ */
+
+namespace App\Model;
+
+
+use Nette\Utils\Finder;
+
+class GalleryManager extends BaseManager
+{
+
+	const GALLERY_DEFAULT = 'Gallery';
+
+	private $masks = ['*.jpg', '*.jpeg', '*.png', '*.gif'];
+
+	public function getImages()
+	{
+		return Finder::findFiles($this->masks)->in(self::GALLERY_DEFAULT . '/default');
+	}
+
+
+	public function getImagesByName($name)
+	{
+		return Finder::findFiles($this->masks)->in(self::GALLERY_DEFAULT . '/' . $this->fileToFolderName($name));
+	}
+
+
+	private function fileToFolderName($name)
+	{
+		return trim(str_replace(' ', '', ucwords(str_replace('-', ' ', str_replace('_', ' ', pathinfo($name, PATHINFO_FILENAME))))));
+	}
+}
